282ac65564073bca0f2c5d543f8667f66f27a911
Cheng-py <2238390982@qq.com>
2021-08-11 15:00:30 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -1,35 +1,119 @@
 from flask import Flask,render_template
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
+
+# app = Flask(__name__)
+# name = 'Old C'
+# movies = [
+# {'title': 'My Neighbor Totoro', 'year': '1988'},
+# {'title': 'Dead Poets Society', 'year': '1989'},
+# {'title': 'A Perfect World', 'year': '1993'},
+# {'title': 'Leon', 'year': '1994'},
+# {'title': 'Mahjong', 'year': '1996'},
+# {'title': 'Swallowtail Butterfly', 'year': '1996'},
+# {'title': 'King of Comedy', 'year': '1999'},
+# {'title': 'Devils on the Doorstep', 'year': '1999'},
+# {'title': 'WALL-E', 'year': '2008'},
+# {'title': 'The Pork of Music', 'year': '2012'},
+# ]
+# @app.route('/')
+# def index():
+#     return render_template('index.html', name=name, movies=movies)
+#
+# @app.route("/user/<name>")
+# def user(name):
+#     return "Hello %s" %name
+#
+# @app.route("/test")
+# def test_url_for():
+#     print(url_for("hello"))
+#     print(url_for("user",name='HaHaHa'))
+#     print(url_for("user",name="LaLaLa"))
+#     print(url_for("test_url_for"))
+#     print(url_for("test_url_for",num = 2))
+#     return "Test Page"
+
+
+WIN = sys.platform.startswith("win")
+if WIN:
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
 
 app = Flask(__name__)
-name = 'Old C'
-movies = [
-{'title': 'My Neighbor Totoro', 'year': '1988'},
-{'title': 'Dead Poets Society', 'year': '1989'},
-{'title': 'A Perfect World', 'year': '1993'},
-{'title': 'Leon', 'year': '1994'},
-{'title': 'Mahjong', 'year': '1996'},
-{'title': 'Swallowtail Butterfly', 'year': '1996'},
-{'title': 'King of Comedy', 'year': '1999'},
-{'title': 'Devils on the Doorstep', 'year': '1999'},
-{'title': 'WALL-E', 'year': '2008'},
-{'title': 'The Pork of Music', 'year': '2012'},
-]
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] =prefix + os.path.join(app.root_path, 'data.sqlite3')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)  # 在拓展类实例化前加载配置
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+@app.context_processor
+def inject_user(): # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user) # 需要返回字典， 等同于return {'user': user}
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html',movies=movies)
 
-@app.route("/user/<name>")
-def user(name):
-    return "Hello %s" %name
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html') ,404
 
-@app.route("/test")
-def test_url_for():
-    print(url_for("hello"))
-    print(url_for("user",name='HaHaHa'))
-    print(url_for("user",name="LaLaLa"))
-    print(url_for("test_url_for"))
-    print(url_for("test_url_for",num = 2))
-    return "Test Page"
+
 
 
